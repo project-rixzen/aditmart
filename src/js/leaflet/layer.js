f89b74f21df7ea649c7987549a39bc1a195c980d
project-rixzen <rixzen99@gmail.com>
2024-12-14 23:28:12 +0700

    feat(maps): update jakbar map geojson

--- src/js/leaflet/layer.js
+++ src/js/leaflet/layer.js
@@ -1,14 +1,13 @@
-var school = new L.LayerGroup();
+var indomaret = new L.LayerGroup();
 var jalan = new L.LayerGroup();
-var tebet = new L.LayerGroup();
-var tebetBorder = new L.LayerGroup();
-var jaksel = new L.LayerGroup();
+var kelurahan = new L.LayerGroup();
+var sungai = new L.LayerGroup();
 
 var map = L.map('map', {
-    center: [-6.237050264556689, 106.85301062524744],
+    center: [-6.152774156483126, 106.78470822205712],
     zoom: 13,
     zoomControl: false,
-    layers: [school, tebetBorder]
+    layers: [indomaret]
 });
 
 var GoogleMaps = new L.TileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
@@ -40,10 +39,10 @@ var baseLayers = {
 
 var groupedOverlays = {
     "Peta Dasar": {
-        'Sekolah': school,
-        'Jalan Jakarta Selatan': jalan,
-        'Tebet': tebet,
-        'Kota Jakarta Selatan': jaksel,
+        'Indomaret': indomaret,
+        'Kelurahan': kelurahan,
+        'Jalan Jakarta Barat': jalan,
+        'Sungai Jakarta Barat': sungai,
     }
 };
 
@@ -56,7 +55,7 @@ GEOJSON LAYER
 var baseUrl = window.location.origin;
 console.log(baseUrl);
 
-$.getJSON(baseUrl + '/src/assets/gis/geojson/titik-sekolah.geojson', function (data) {
+$.getJSON(baseUrl + '/src/assets/gis/geojson/indomaret_locations.geojson', function (data) {
     var ratIcon = L.icon({
         iconUrl: '/src/assets/gis/marker.png',
         iconSize: [24, 24]
@@ -64,20 +63,20 @@ $.getJSON(baseUrl + '/src/assets/gis/geojson/titik-sekolah.geojson', function (d
     L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
             var marker = L.marker(latlng, { icon: ratIcon });
-            marker.bindPopup(feature.properties.NAMA_SEKOLAH);
+            marker.bindPopup(feature.properties.name);
             return marker;
         }
-    }).addTo(school);
+    }).addTo(indomaret);
 });
 
-$.getJSON(baseUrl +'/src/assets/gis/geojson/jalan-jaksel.geojson', function (data) {
+$.getJSON(baseUrl +'/src/assets/gis/geojson/jalan_jakarta_barat.geojson', function (data) {
     L.geoJson(data, {
         style: function (feature) {
             var color,
                 kode = feature.properties.kode;
-            if (kode < 2) color = "#f2051d";
-            else if (kode > 0) color = "#f2051d";
-            else color = "#f2051d"; // no data
+            if (kode < 2) color = "#707070";
+            else if (kode > 0) color = "#707070";
+            else color = "#707070"; // no data
             return { color: color, weight: 1, fillOpacity: 0.8 };
         },
         onEachFeature: function (feature, layer) {
@@ -86,89 +85,32 @@ $.getJSON(baseUrl +'/src/assets/gis/geojson/jalan-jaksel.geojson', function (dat
     }).addTo(jalan); 
 });
 
-
-$.getJSON(baseUrl + '/src/assets/gis/geojson/polygon-tebet.geojson', function (kode) {
-    L.geoJson(kode, {
+$.getJSON(baseUrl +'/src/assets/gis/geojson/sungai_jakarta_barat.geojson', function (data) {
+    L.geoJson(data, {
         style: function (feature) {
-            var fillColor,
-                kode = feature.properties.id;
-            if (kode > 21) fillColor = "#006837";
-            else if (kode > 20) fillColor = "#fec44f"
-            else if (kode > 19) fillColor = "#c2e699"
-            else if (kode > 18) fillColor = "#fee0d2"
-            else if (kode > 17) fillColor = "#756bb1"
-            else if (kode > 16) fillColor = "#8c510a"
-            else if (kode > 15) fillColor = "#01665e"
-            else if (kode > 14) fillColor = "#e41a1c"
-            else if (kode > 13) fillColor = "#636363"
-            else if (kode > 12) fillColor = "#762a83"
-            else if (kode > 11) fillColor = "#1b7837"
-            else if (kode > 10) fillColor = "#d53e4f"
-            else if (kode > 9) fillColor = "#67001f"
-            else if (kode > 8) fillColor = "#c994c7"
-            else if (kode > 7) fillColor = "#fdbb84"
-            else if (kode > 6) fillColor = "#dd1c77"
-            else if (kode > 5) fillColor = "#3182bd"
-            else if (kode > 4) fillColor = "#f03b20"
-            else if (kode > 3) fillColor = "#31a354";
-            else if (kode > 2) fillColor = "#78c679";
-            else if (kode > 1) fillColor = "#c2e699";
-            else if (kode > 0) fillColor = "#ffffcc";
-            else fillColor = "#f7f7f7"; // no data
-            return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .6 };
+            var color,
+                kode = feature.properties.kode;
+            if (kode < 2) color = "#0000FF";
+            else if (kode > 0) color = "#0000FF";
+            else color = "#0000FF"; // no data
+            return { color: color, weight: 1, fillOpacity: 0.8 };
         },
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(feature.properties.tebet)
+            layer.bindPopup("Informasi yang ingin ditampilkan");
         }
-    }).addTo(tebet);
-});
-
-$.getJSON(baseUrl + '/src/assets/gis/geojson/polygon-jaksel.geojson', function (kode) {
-    L.geoJson( kode, {
-        style: function(feature){
-            var fillColor,
-                kode = feature.properties.id;
-            if ( kode > 21 ) fillColor = "#006837";
-            else if (kode>20) fillColor="#fec44f"
-            else if (kode>19) fillColor="#c2e699"
-            else if (kode>18) fillColor="#fee0d2"
-            else if (kode>17) fillColor="#756bb1"
-            else if (kode>16) fillColor="#8c510a"
-            else if (kode>15) fillColor="#01665e"
-            else if (kode>14) fillColor="#e41a1c"
-            else if (kode>13) fillColor="#636363"
-            else if (kode>12) fillColor= "#762a83"
-            else if (kode>11) fillColor="#1b7837"
-            else if (kode>10) fillColor="#d53e4f"
-            else if (kode>9) fillColor="#67001f"
-            else if (kode>8) fillColor="#c994c7"
-            else if (kode>7) fillColor="#fdbb84"
-            else if (kode>6) fillColor="#dd1c77"
-            else if (kode>5) fillColor="#3182bd"
-            else if ( kode > 4 ) fillColor ="#f03b20"
-            else if ( kode > 3 ) fillColor = "#31a354";
-            else if ( kode > 2 ) fillColor = "#78c679";
-            else if ( kode > 1 ) fillColor = "#c2e699";
-            else if ( kode > 0 ) fillColor = "#ffffcc";
-            else fillColor = "#f7f7f7"; // no data
-            return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .6 };
-        },
-        onEachFeature: function( feature, layer ){
-            layer.bindPopup(feature.properties.jaksel)
-        }
-    }).addTo(jaksel);
+    }).addTo(sungai); 
 });
 
 
-$.getJSON(baseUrl + '/src/assets/gis/geojson/polygon-tebet.geojson', function (kode) {
+$.getJSON(baseUrl + '/src/assets/gis/geojson/batas_kelurahan.geojson', function (kode) {
     L.geoJson(kode, {
         style: function (feature) {
-            return { color: "orange", weight: 3, fillOpacity: 0, dashArray: '5' }; // Orange dashed border, no fill
+            return { color: "#FF5733", weight: 3, fillOpacity: 0 }; // orang border, transparent fill, no dashes
         },
         onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.tebet);
         }
-    }).addTo(tebetBorder);
+    }).addTo(kelurahan);
 });
 
 
